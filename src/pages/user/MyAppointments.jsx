87b45d5695f25ccfc4917e5dec8fc01e2aae3b60
sairamanuja <sairamanuja789@gmail.com>
2025-04-21 10:26:16 +0530

    added changes in vercel.json

--- src/pages/user/MyAppointments.jsx
+++ src/pages/user/MyAppointments.jsx
@@ -71,10 +71,7 @@ const Appointments = () => {
                 ))}
               </div>
 
-              {/* Cancel Appointment Button */}
-              <button className="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition-colors">
-                Cancel Appointment
-              </button>
+            
             </div>
           ))}
         </div>
